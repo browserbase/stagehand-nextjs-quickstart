d9a410da07af675476435bf1dd4c60681ab04464
Anirudh Kamath <github@kamath.io>
2025-02-02 22:55:23 -0800

    Revert "goto + observe"
    This reverts commit c1000a0ed810d2166a4e63970578534090ee3368.

--- app/api/stagehand/run.ts
+++ app/api/stagehand/run.ts
@@ -1,12 +1,18 @@
 /**
- * This is the server-side entry point for Stagehand/Browserbase.
+ * 🤘 Welcome to Stagehand!
+ *
+ * This is the server-side entry point for Stagehand.
+ *
+ * To edit the Stagehand script, see `api/stagehand/main.ts`.
+ * To edit config, see `stagehand.config.ts`.
+ *
+ * In this quickstart, we'll be automating a browser session to show you the power of Playwright and Stagehand's AI features.
  */
-
 "use server";
 
 import StagehandConfig from "@/stagehand.config";
 import Browserbase from "@browserbasehq/sdk";
-import { ObserveResult, Page, Stagehand } from "@browserbasehq/stagehand";
+import { Stagehand } from "@browserbasehq/stagehand";
 import { main } from "./main";
 
 export async function runStagehand(sessionId?: string) {
@@ -20,25 +26,15 @@ export async function runStagehand(sessionId?: string) {
 }
 
 export async function startBBSSession() {
-  const browserbase = new Browserbase();
+  const browserbase = new Browserbase(StagehandConfig);
   const session = await browserbase.sessions.create({
     projectId: StagehandConfig.projectId!,
-    keepAlive: true,
   });
   const debugUrl = await browserbase.sessions.debug(session.id);
   return {
     sessionId: session.id,
     debugUrl: debugUrl.debuggerFullscreenUrl,
   };
-}
-
-export async function stopBBSSession(sessionId: string) {
-  console.log("Stopping session", sessionId);
-  const browserbase = new Browserbase();
-  await browserbase.sessions.update(sessionId, {
-    projectId: StagehandConfig.projectId!,
-    status: "REQUEST_RELEASE",
-  });
 }
 
 export async function getConfig() {
@@ -57,94 +53,4 @@ export async function getConfig() {
     hasBrowserbaseCredentials,
     hasLLMCredentials,
   };
-}
-
-async function drawObserveOverlay(page: Page, xpath: string) {
-  await page.evaluate((selector) => {
-    let element;
-    if (selector.startsWith("xpath=")) {
-      const xpath = selector.substring(6);
-      element = document.evaluate(
-        xpath,
-        document,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        null
-      ).singleNodeValue;
-    } else {
-      element = document.querySelector(selector);
-    }
-    if (element instanceof HTMLElement) {
-      // Create overlay
-      const overlay = document.createElement("div");
-      // Add stagehandObserve attribute
-      overlay.setAttribute("stagehandObserve", "true");
-      const rect = element.getBoundingClientRect();
-      overlay.style.position = "absolute";
-      overlay.style.left = rect.left + "px";
-      overlay.style.top = rect.top + "px";
-      overlay.style.width = rect.width + "px";
-      overlay.style.height = rect.height + "px";
-      overlay.style.backgroundColor = "rgba(255, 255, 0, 0.3)";
-      overlay.style.pointerEvents = "none";
-      overlay.style.zIndex = "10000";
-      document.body.appendChild(overlay);
-    }
-  }, xpath);
-}
-
-export async function goto(url: string, sessionId?: string) {
-  const stagehand = new Stagehand({
-    ...StagehandConfig,
-    browserbaseSessionID: sessionId,
-  });
-  await stagehand.init();
-  await stagehand.page.goto(url);
-  await stagehand.close();
-}
-
-export async function observe(instruction: string, sessionId?: string) {
-  const stagehand = new Stagehand({
-    ...StagehandConfig,
-    browserbaseSessionID: sessionId,
-  });
-  await stagehand.init();
-  const results = await stagehand.page.observe({
-    instruction,
-    onlyVisible: false,
-    returnAction: true,
-  });
-  await Promise.all(
-    results.map(
-      async (result) =>
-        await drawObserveOverlay(stagehand.page, result.selector)
-    )
-  );
-  await stagehand.close();
-  return results;
-}
-
-export async function getSuggestions(instruction?: string, sessionId?: string) {
-  const suggestions = await observe(
-    "Get me the top 3 things that a user would want to do or read on this page, with the most likely first. Phrase the suggestions as ACTIONS, rather than as selectors. " +
-      (instruction
-        ? ` You want to achieve the following goal: "${instruction}"`
-        : ""),
-    sessionId!
-  );
-  console.log("SUGGESTIONS", suggestions);
-  return suggestions;
-}
-
-export async function actFromObserve(
-  observeResult: ObserveResult,
-  sessionId: string
-) {
-  const stagehand = new Stagehand({
-    ...StagehandConfig,
-    browserbaseSessionID: sessionId,
-  });
-  await stagehand.init();
-  await stagehand.page.act(observeResult);
-  await stagehand.close();
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,22 @@
 "use client";
 
-import { getConfig } from "@/app/api/stagehand/run";
 import {
-  debugUrlAtom,
-  errorAtom,
-  runningAtom,
-  sessionIdAtom,
-  warningAtom,
-} from "@/atoms";
-import ErrorOrWarning from "@/components/client/Error";
-import Start from "@/components/client/Start";
+  getConfig,
+  runStagehand,
+  startBBSSession,
+} from "@/app/api/stagehand/run";
 import DebuggerIframe from "@/components/stagehand/debuggerIframe";
 import { ConstructorParams } from "@browserbasehq/stagehand";
-import { useAtom, useAtomValue } from "jotai";
 import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [config, setConfig] = useState<ConstructorParams | null>(null);
-  const sessionId = useAtomValue(sessionIdAtom);
-  const debugUrl = useAtomValue(debugUrlAtom);
-  const error = useAtomValue(errorAtom);
-  const [warning, setWarning] = useAtom(warningAtom);
-  const running = useAtomValue(runningAtom);
+  const [running, setRunning] = useState(false);
+  const [debugUrl, setDebugUrl] = useState<string | undefined>(undefined);
+  const [sessionId, setSessionId] = useState<string | undefined>(undefined);
+  const [error, setError] = useState<string | null>(null);
+  const [warning, setWarning] = useState<string | null>(null);
 
   const fetchConfig = useCallback(async () => {
     const config = await getConfig();
@@ -39,7 +33,28 @@ export default function Home() {
       );
     }
     setWarning(warningToShow.join("\n"));
-  }, [setWarning]);
+  }, []);
+
+  const startScript = useCallback(async () => {
+    if (!config) return;
+
+    setRunning(true);
+
+    try {
+      if (config.env === "BROWSERBASE") {
+        const { sessionId, debugUrl } = await startBBSSession();
+        setDebugUrl(debugUrl);
+        setSessionId(sessionId);
+        await runStagehand(sessionId);
+      } else {
+        await runStagehand();
+      }
+    } catch (error) {
+      setError((error as Error).message);
+    } finally {
+      setRunning(false);
+    }
+  }, [config]);
 
   useEffect(() => {
     fetchConfig();
@@ -79,8 +94,61 @@ export default function Home() {
           </li>
         </ul>
 
-        <Start sessionId={sessionId} />
-        <ErrorOrWarning error={error} warning={warning} />
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          {!running && (
+            <a
+              href="#"
+              className=" border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 hover:bg-yellow-500"
+              onClick={startScript}
+            >
+              🤘 Run Stagehand
+            </a>
+          )}
+          {sessionId && (
+            <a
+              href={`https://www.browserbase.com/sessions/${sessionId}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="border border-solid transition-colors flex items-center justify-center bg-[#F9F6F4] text-black gap-2 hover:border-[#F7F7F7] hover:text-black text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 group "
+            >
+              <div className="relative w-4 h-4">
+                <Image
+                  src="/browserbase_grayscale.svg"
+                  alt="Browserbase"
+                  width={16}
+                  height={16}
+                  className="absolute opacity-0 group-hover:opacity-100 transition-opacity"
+                />
+                <Image
+                  src="/browserbase.svg"
+                  alt="Browserbase"
+                  width={16}
+                  height={16}
+                  className="absolute group-hover:opacity-0 transition-opacity"
+                />
+              </div>
+              View Session on Browserbase
+            </a>
+          )}
+          <a
+            className="border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://docs.stagehand.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+        {error && (
+          <div className="bg-red-400 text-white rounded-md p-2 max-w-lg">
+            Error: {error}
+          </div>
+        )}
+        {warning && (
+          <div className="bg-yellow-400 text-black rounded-md p-2 max-w-lg">
+            <strong>Warning:</strong> {warning}
+          </div>
+        )}
       </main>
     </div>
   );
